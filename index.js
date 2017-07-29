b991d8dbbe0c70b3cda9c5ebb0a49b5f84068d59
Arjun  Dass <ad.sam007@gmail.com>
2017-07-29 13:42:53 -0500

    Added requirements

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+
+// function that returns the list of files in a folder
+
+// funtion that returns the number of files ina folder
+
+// function that returns the type of files in a folder
+
+// function that renames the files in a folder
+
+// function that renames a specific file in a folder
+
+// function that create a files in a folder
+
+// function that removes a file from a folder
+
+// function that removes all files of a particular type from a folder
+
+// fuction that checks if a specific files exists in a folder
+
+// function that change the names of all the files in a folder with incremental value
+
+// function that change the extensions of all the files in a folder
+
+// function that change the extension of a particular file in a folder
+
+// functon that create a files (Programming or txt);
+
+// function that renames a folder
+
+// function that returns the system path of a file
+
+// function that moves a files from one folder to another
+
+// function that create a copy of a files 
+
+// function to copy the contents of a files of one extesion to another
