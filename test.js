19731aa04642b4665e64617f9efdc8445571d1ab
Arjun  Dass <ad.sam007@gmail.com>
2017-08-26 12:50:29 -0500

    Added mocha for testing

--- test.js
+++
@@ -1,1 +0,0 @@
-var alterfiles = require('./index.js');
---
+++ test/test.js
@@ -0,0 +1,13 @@
+var alterfiles = require('./../alterfiles.js');
+var expect = require('expect');
+var assert = require('assert'); 
+
+describe('Main Application', () => {
+    
+    it('should  properly run tests', () => {
+        expect(1).toBe(1);
+    });
+
+
+});
+
