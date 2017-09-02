7602a74325d611eb2292ca8ad4b17ca3bd97f920
Arjun  Dass <ad.sam007@gmail.com>
2017-09-02 12:26:10 -0500

    added test files

--- alterfiles.js
+++ alterfiles.js
@@ -514,6 +514,27 @@ const createCopy = (path, filename, newFilename) => {
     }
 }
 
+const csvToJSON = (path) => {
+    var lines=csv.split("\n");
+    
+      var result = [];
+    
+      var headers=lines[0].split(",");
+    
+      for(var i=1;i<lines.length;i++){
+          var obj = {};
+          var currentline=lines[i].split(",");
+    
+          for(var j=0;j<headers.length;j++){
+              obj[headers[j]] = currentline[j];
+          }
+          result.push(obj);
+      }
+
+      console.log(JSON.stringify(result));
+      return JSON.stringify(result);
+}
+
 module.exports = {
 
     allFiles: allFiles,
